bf6130c13b0a2a72dc930cf22d424bee8a314527
Dominic Brown and Jennifer Kinsey <jennifer@>
2017-03-23 13:27:50 -0700

    debug script, finish polishing js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
 
+  $(".tri-form").submit(function(event){
 
-  var equilateralBool = (sideA === sideB && sideB === sideC);
-  var isoscelesBool = (sideA === sideB || sideA === sideC || sideB === sideC);
+    var triangleType = "(empty before if statements)";
+    var tri1Input = parseInt($("input#sideA").val());
+    var tri2Input = parseInt($("input#sideB").val());
+    var tri3Input = parseInt($("input#sideC").val());
 
-  var scaleneBool = (sideA !== sideB && sideA !== sideC && sideB !== sideC);
+    if (((tri1Input + tri2Input) <= tri3Input) || ((tri1Input + tri3Input) <= tri2Input) || ((tri2Input + tri3Input) <= tri1Input)) { triangleType = "not a triangle";
+    } else if ((tri1Input === tri2Input) && (tri2Input === tri3Input)) {
+      triangleType = "equilateral";
+    } else if ((tri1Input === tri2Input) || (tri1Input === tri3Input) || (tri2Input === tri3Input)) {
+      triangleType = "isosceles";
+    } else if (tri1Input !== tri2Input && tri1Input !== tri3Input && tri2Input !== tri3Input) {
+      triangleType = "scalene";
+    } else {
+      alert("contact the devs!");
+    }
 
-  var nontriBool = (((sideA + sideB) <= sideC) || ((sideA + sideC) <= sideB) || ((sideB + sideC) <= sideA));
+    $("#infoSpan").text(triangleType);
+
+    $(".tri-info").show();
+    event.preventDefault();
+  });
 
 });
